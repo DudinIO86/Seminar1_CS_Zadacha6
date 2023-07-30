4453b3a3eb6bcbeb443625c0f96ddc604f6b20ee
Igor Dudin <dudinio@mail.ru>
2023-07-30 13:48:27 +0300

    Работающий код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Введите число: ");
+string strNum=Console.ReadLine();
+int num=int.Parse(strNum);
+
+if(num%2==0)
+{
+    Console.WriteLine($"Число {num} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {num} не является четным");
+}
